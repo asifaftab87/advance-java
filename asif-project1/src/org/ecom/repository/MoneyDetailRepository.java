68e40383ec481fee7f57c751e5ff2d1a62badf19
fayez.alsaif <fayez.alsaif@SRPC-ARR-D100.srmg.sa>
2020-08-10 02:15:03 +0300

    added MoneyDetail Repository

--- asif-project1/src/org/ecom/repository/MoneyDetailRepository.java
+++ asif-project1/src/org/ecom/repository/MoneyDetailRepository.java
@@ -8,117 +8,230 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.ecom.model.Address;
+import org.ecom.model.MoneyDetail;
+
 
 public class MoneyDetailRepository {
 
-	public static List<Address> getAllAddress(Connection con) {
+	public static List<MoneyDetail> getAllMoneyDetail(Connection con) {
 
-		System.out.println("-----------getAllAddress------------");
-		
+		System.out.println("-----------getAllMoneyDetail------------");
+
 		Statement stmt = null;
 		ResultSet rs = null;
 
-	
-		List<Address> addressList = new ArrayList<>();
+		List<MoneyDetail> MoneyDetailList = new ArrayList<>();
 		try {
 			stmt = con.createStatement();
-			rs = stmt.executeQuery(" select * from address ");
-			
-			if(rs!=null) {
-				
-				while(rs.next())  {
-					  
-					Address address = new Address();
-					address.setId(rs.getInt(1));
-					address.setUserId(rs.getInt(2));
-					address.setStreet(rs.getString(3));
-					address.setCity(rs.getString(4));
-					address.setState(rs.getString(5));
-					address.setCountry(rs.getString(6));
-					address.setPincode(rs.getString(7));
-					addressList.add(address);
+			rs = stmt.executeQuery(" select * from MoneyDetail ");
+
+			if (rs != null) {
+
+				while (rs.next()) {
+
+					MoneyDetail moneydet = new MoneyDetail();
+					moneydet = new MoneyDetail();
+					moneydet.setId(rs.getInt(1));
+					moneydet.setUserid(rs.getInt(2));
+					moneydet.setBalance(rs.getDouble(3));
+					moneydet.setCreatedate(rs.getDate(4));
+					moneydet.setUpdatedate(rs.getDate(5));
+					MoneyDetailList.add(moneydet);
 				}
-				
+
 			}
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		finally {
+		} finally {
 			try {
-				if(stmt!=null) {
+				if (stmt != null) {
 					stmt.close();
 				}
-			} 
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
-			}  
+			}
 			try {
-				if(rs!=null) {
+				if (rs != null) {
 					rs.close();
 				}
-			} 
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
-			}  
+			}
 		}
-		return addressList;
+		return MoneyDetailList;
 	}
-	
-	public static List<Address> findAddressByUserId(Connection con, long userId) {
 
-		System.out.println("-----------findAddressByUserId userid: "+userId);
+	public static List<MoneyDetail> findMoneyDetailByUserId(Connection con, long userId) {
+
+		System.out.println("-----------findMoneyDetailByUserId userid: " + userId);
 
 		ResultSet rs = null;
-		
-		
-		List<Address> addressList = new ArrayList<>();
+
+		List<MoneyDetail> MoneyDetailList = new ArrayList<>();
 		PreparedStatement pStatement = null;
-		
+
 		try {
-			String query = " select * from address where userid=? ";
+			String query = " select * from MoneyDetail where userid=? ";
 			pStatement = con.prepareStatement(query);
 			pStatement.setLong(1, userId);
 			rs = pStatement.executeQuery();
-			
-			if(rs!=null) {
-				
-				while(rs.next())  {
-					Address address = new Address();
-					address.setId(rs.getInt(1));
-					address.setUserId(rs.getInt(2));
-					address.setStreet(rs.getString(3));
-					address.setCity(rs.getString(4));
-					address.setState(rs.getString(5));
-					address.setCountry(rs.getString(6));
-					address.setPincode(rs.getString(7));
-					addressList.add(address);
+
+			if (rs != null) {
+
+				while (rs.next()) {
+					MoneyDetail moneydet = new MoneyDetail();
+					moneydet.setId(rs.getInt(1));
+					moneydet.setUserid(rs.getInt(2));
+					moneydet.setBalance(rs.getDouble(3));
+					moneydet.setCreatedate(rs.getDate(4));
+					moneydet.setUpdatedate(rs.getDate(5));
+					MoneyDetailList.add(moneydet);
 				}
 			}
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		finally {
+		} finally {
 			try {
-				if(pStatement!=null) {
+				if (pStatement != null) {
 					pStatement.close();
 				}
-			} 
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
-			}  
+			}
 			try {
-				if(rs!=null) {
+				if (rs != null) {
 					rs.close();
 				}
-			} 
-			catch (SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
-			}  
+			}
 		}
-		return addressList;
+		return MoneyDetailList;
+	}
+
+	// create method MoneyDetail
+	public static void createMoneyDetail(Connection con, MoneyDetail moneydet) {
+
+		System.out.println("-----------createMoneyDetail------------");
+
+		PreparedStatement pStatement = null;
+
+		try {
+
+			// conversion from java.util.Date to java.sql.Date
+			java.sql.Date sqlCreatDate = new java.sql.Date(moneydet.getCreatedate().getTime());
+			java.sql.Date sqlUpdatDate = new java.sql.Date(moneydet.getUpdatedate().getTime());
+
+			String query = "INSERT INTO MoneyDetail(balance, createdate , updatedate ) " + " VALUES (?, ?, ?)";
+			pStatement = con.prepareStatement(query);
+			pStatement.setDouble(1, moneydet.getBalance());
+			pStatement.setDate(2, sqlCreatDate);
+			pStatement.setDate(3, sqlUpdatDate);
+
+			int executeUpdate = pStatement.executeUpdate();
+
+			if (executeUpdate > 0) {
+				System.out.println("data created successfully: " + executeUpdate);
+			} else {
+				System.out.println("failed to insert data: " + executeUpdate);
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (pStatement != null) {
+					pStatement.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+ //update MoneyDetail
+	public static void updateMoneyDetail(Connection con, MoneyDetail moneydet) {
+
+		System.out.println("-----------updateMoneyDetail------------");
+
+		PreparedStatement pStatement = null;
+
+		try {
+
+			// conversion from java.util.Date to java.sql.Date
+			java.sql.Date sqlCreatDate = new java.sql.Date(moneydet.getCreatedate().getTime());
+			java.sql.Date sqlUpdatDate = new java.sql.Date(moneydet.getUpdatedate().getTime());
+
+			String query = 	  " update MoneyDetail set balance=?, createdate=?, updatedate=?"
+					+ " where id=? ";
+			
+			pStatement = con.prepareStatement(query);
+			pStatement.setDouble(1, moneydet.getBalance());
+			pStatement.setDate(2, sqlCreatDate);
+			pStatement.setDate(3, sqlUpdatDate);
+
+			int executeUpdate = pStatement.executeUpdate();
+
+			if (executeUpdate > 0) {
+				System.out.println("data created successfully: " + executeUpdate);
+			} else {
+				System.out.println("failed to insert data: " + executeUpdate);
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (pStatement != null) {
+					pStatement.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
+	//delete MoneyDetail
+	public static void deleteMoneyDetail(Connection con, MoneyDetail moneydet) {
+
+		System.out.println("-----------deleteMoneyDetailByID MoneyDetailid: "+moneydet);
+
+		PreparedStatement pStatement = null;
+
+		try {
+
+			// conversion from java.util.Date to java.sql.Date
+			java.sql.Date sqlCreatDate = new java.sql.Date(moneydet.getCreatedate().getTime());
+			java.sql.Date sqlUpdatDate = new java.sql.Date(moneydet.getUpdatedate().getTime());
+
+			String query = "delete from MoneyDetail where id=?";
+			
+			pStatement = con.prepareStatement(query);
+			pStatement.setDouble(1, moneydet.getBalance());
+			pStatement.setDate(2, sqlCreatDate);
+			pStatement.setDate(3, sqlUpdatDate);
+
+			int executeUpdate = pStatement.executeUpdate();
+
+			if (executeUpdate > 0) {
+				System.out.println("data created successfully: " + executeUpdate);
+			} else {
+				System.out.println("failed to insert data: " + executeUpdate);
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (pStatement != null) {
+					pStatement.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
+	
